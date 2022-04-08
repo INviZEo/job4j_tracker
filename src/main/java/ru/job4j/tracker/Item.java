a4b6b3f611141040b881fb76bdd8b0484303605d
INviZEo <lev19825@mail.ru>
2022-04-09 00:26:31 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,20 @@
 package ru.job4j.tracker;
 
 public class Item {
+
     private int id;
     private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+
+    }
+
+    public Item(int id, String name) {
+
+    }
 
     public int getId() {
         return id;
