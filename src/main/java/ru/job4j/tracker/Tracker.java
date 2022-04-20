1503a7179f15cc90fef94e378dc4598c6a4a6824
INviZEo <lev19825@mail.ru>
2022-04-21 02:55:48 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,20 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        int f = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                items[f++] = item;
+            }
+        }
+        return Arrays.copyOf(items, f);
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
     }
 }
