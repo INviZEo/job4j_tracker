44842e08789b3f33a48f6aff79cdaa436e57c6d1
INviZEo <lev19825@mail.ru>
2022-05-02 04:33:14 +0300

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -9,7 +9,7 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        boolean del = index != 1;
+        boolean del = index != -1;
         if (del) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
