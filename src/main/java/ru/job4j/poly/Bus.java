529fe31615e7ed77298b11dd3b74731906b56157
INviZEo <lev19825@mail.ru>
2022-05-04 12:58:47 +0300

    3. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,10 +3,12 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void ride() {
+        String onWhat = "Bicycle";
     }
 
     @Override
     public void passengers(int quantityHuman) {
+        quantityHuman = 10;
     }
 
     @Override
