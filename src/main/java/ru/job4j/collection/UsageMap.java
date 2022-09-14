1d753b768e03dcf49b2a3dce173323f477527f5f
INviZEo <lev19825@mail.ru>
2022-09-14 16:42:12 +0300

    0. Map, HashMap [#212652 #343082]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,9 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("lev2281488322@gmail.com", "Lev Pasternak");
-        for (String d: map.keySet()) {
-            String key = map.get(d);
-            System.out.println(key);
+        for (String d : map.keySet()) {
+            String value = map.get(d);
+            System.out.println(value);
             System.out.println(d);
         }
     }
