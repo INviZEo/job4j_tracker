1d86eb72caecf73641521d3bb2fe088419871b90
INviZEo <lev19825@mail.ru>
2023-07-05 12:04:14 +0300

    3. Компаратор для строк [#35008 # [#35008]]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -8,8 +8,8 @@ public class StringCompare implements Comparator<String> {
         for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
             if (left.charAt(i) != right.charAt(i)) {
                 return Character.compare(left.charAt(i), right.charAt(i));
-                }
             }
+        }
         return Integer.compare(left.length(), right.length());
     }
 }
