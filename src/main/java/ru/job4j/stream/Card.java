76d7f587a26878dfbfea4870b0bd7c074d9483d4
INviZEo <lev19825@mail.ru>
2023-10-24 22:10:08 +0300

    9. Генерирование колоды карт. FlatMap [#504873]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -8,7 +8,7 @@ public class Card {
     private Suit suit;
     private Value value;
 
-    public Card(Suit suit, Value value){
+    public Card(Suit suit, Value value) {
         this.suit = suit;
         this.value = value;
     }
