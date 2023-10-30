2ff6ce872d74880232420e1f9b027f98a365fed9
INviZEo <lev19825@mail.ru>
2023-10-30 03:53:21 +0300

    3. Optional - empty, of [#214711 #456277]

---
+++ src/main/java/ru/job4j/collection/College.java
@@ -0,0 +1,38 @@
+package ru.job4j.collection;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class College {
+
+    private final Map<Student, Set<Subject>> students;
+
+    public College(Map<Student, Set<Subject>> students) {
+        this.students = students;
+    }
+
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl = Optional.empty();
+        for (Student s : students.keySet()) {
+            if (s.account().equals(account))
+                rsl = Optional.of(s);
+                break;
+        }
+        return students.keySet()
+                .stream()
+                .filter(s -> s.account().equals(account))
+                .findFirst();
+    }
+
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Student> std = findByAccount(account);
+        if (std.isEmpty()) {
+            return Optional.empty();
+        }
+        return students.get(std.get())
+                .stream()
+                .filter(student -> student.name().equals(name))
+                .findFirst();
+    }
+}
