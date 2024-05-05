34584198287815adbe033b4a9443e2b20917d68e
INviZEo <lev19825@mail.ru>
2024-05-05 21:06:45 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851]

---
+++ src/main/java/ru/job4j/ood/ocp/ReportJSON.java
@@ -0,0 +1,33 @@
+package ru.job4j.ood.ocp;
+
+import com.google.gson.GsonBuilder;
+import ru.job4j.ood.srp.currency.Currency;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.report.Report;
+import ru.job4j.ood.srp.store.Store;
+
+import java.util.Calendar;
+import java.util.function.Predicate;
+
+public class ReportJSON implements Report {
+
+    private final Store store;
+    private final DateTimeParser<Calendar> dateTimeParser;
+    private final GsonBuilder gsonBuilder;
+
+    public ReportJSON(Store store, DateTimeParser<Calendar> dateTimeParser, GsonBuilder gsonBuilder) {
+        this.store = store;
+        this.dateTimeParser = dateTimeParser;
+        this.gsonBuilder = gsonBuilder;
+    }
+
+    @Override
+    public String generate(Predicate<Employee> filter) {
+        StringBuilder text = new StringBuilder();
+        for (Employee employee : store.findBy(filter)) {
+            text = new StringBuilder(gsonBuilder.create().toJson(employee));
+        }
+        return text.toString();
+    }
+}
---
+++ src/test/java/ru/job4j/ood/ocp/ReportJSONTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.ood.ocp;
+
+import com.google.gson.GsonBuilder;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemoryStore;
+
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Disabled
+class ReportJSONTest {
+
+    @Test
+    public void testJSON() {
+        MemoryStore store = new MemoryStore();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        ReportJSON reportJSON = new ReportJSON(store, parser, new GsonBuilder());
+        StringBuilder expected = new StringBuilder().append("{")
+                .append("\"Name\"").append(":\"")
+                .append(worker.getName()).append("\",")
+                .append("\"Hired\"").append(":\"")
+                .append(parser.parse(worker.getHired())).append("\",")
+                .append("\"Fired\"").append(":\"")
+                .append(parser.parse(worker.getFired())).append("\",")
+                .append("\"Salary\"").append(":\"")
+                .append(worker.getSalary()).append(":\"")
+                .append("}")
+                .append(System.lineSeparator());
+        assertThat(reportJSON.generate(employee -> true)).isEqualTo(expected);
+    }
+}
